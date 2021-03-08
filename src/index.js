965912c38e22073d108c02f86878fc6aacc1e105
roy putterman <royputterman@gmai.com>
2021-03-08 19:58:47 +0200

    beginning 2

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express=require('express')
+const port  =process.env.PORT || 4000
+const app   =express()
+
+app.get('/',(req ,res)=>{
+    res.send("Welcome To Home Page")
+})
+
+app.get('/LoginPage',(req ,res)=>{
+    res.send("Login Page")
+})
+app.listen(port,()=>{
+    console.log('server is up and running at: http://127.0.0.1:'+port)
+})
